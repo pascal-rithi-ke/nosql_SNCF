fa6552d03d9be4dc654b6528b755412550789f60
Pascal Kien <pascal.rithi@gmail.com>
2023-02-16 16:50:45 +0100

    ajout insertion data collection

--- backend/bdd/config/api/sncf.py
+++ backend/bdd/config/api/sncf.py
@@ -7,9 +7,7 @@ load_dotenv()
 ApiKey = os.getenv('ApiKey')
 
 def departMontparnasse(ApiKey, count):
-
     url = "https://api.sncf.com/v1/coverage/sncf/stop_areas/stop_area:SNCF:87391003/departures"
-
     params = {
     # nombre de résultats à afficher
     "count": count,
@@ -18,7 +16,6 @@ def departMontparnasse(ApiKey, count):
     # votre clé API SNCF
     "key": ApiKey
     }
-
     headers = {
     # spécifier l'en-tête "Accept" pour recevoir les données au format JSON
     "Accept": "application/json"
@@ -27,39 +24,65 @@ def departMontparnasse(ApiKey, count):
     response = requests.get(url, params=params, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        '''for departure in data["departures"]:
-            print(departure["display_informations"]["direction"])'''
         return data
-        #db.test.insert_one(data)
     else:
         print("Une erreur est survenue: {}".format(response.status_code))
         return None
 
 def objetTrouve(ApiKey, count):
-
     url = "https://data.sncf.com/api/records/1.0/search/?dataset=objets-trouves-restitution&q=&sort=date&facet=date&facet=gc_obo_date_heure_restitution_c&facet=gc_obo_gare_origine_r_name&facet=gc_obo_nature_c&facet=gc_obo_type_c&facet=gc_obo_nom_recordtype_sc_c"
-
     params = {
-    # nombre de résultats à afficher
     "count": count,
-    # désactiver la géolocalisation
     "disable_geojson": "true",
-    # votre clé API SNCF
     "key": ApiKey
     }
-
     headers = {
-    # spécifier l'en-tête "Accept" pour recevoir les données au format JSON
     "Accept": "application/json"
     }
 
     response = requests.get(url, params=params, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        '''for departure in data["departures"]:
-            print(departure["display_informations"]["direction"])'''
         return data
-        #db.test.insert_one(data)
+    else:
+        print("Une erreur est survenue: {}".format(response.status_code))
+        return None
+    
+def terMensuel(api_key, count):
+    url = "https://data.sncf.com/api/records/1.0/search/?dataset=regularite-mensuelle-ter&q=&sort=date&facet=date&facet=region"
+    params = {
+    "count": count,
+    "disable_geojson": "true",
+    "key": api_key
+    }
+    headers = {
+    "Accept": "application/json"
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print("Une erreur est survenue: {}".format(response.status_code))
+        return None
+    
+def TGVMensuel(api_key, count):
+    url = "https://ressources.data.sncf.com/api/records/1.0/search/?dataset=regularite-mensuelle-tgv-aqst&q=&sort=date&facet=date&facet=service&facet=gare_depart&facet=gare_arrivee"
+
+    params = {
+    "count": count,
+    "disable_geojson": "true",
+    "key": api_key
+    }
+    headers = {
+    "Accept": "application/json"
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data
     else:
         print("Une erreur est survenue: {}".format(response.status_code))
         return None
--- backend/bdd/mongoDB.py
+++ backend/bdd/mongoDB.py
@@ -5,15 +5,53 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 ApiKey = os.getenv('ApiKey')
+CheckCollection = loadConfigDataBase()
+
+limitData = 10
+
+CollecMontParnasse = "MontParnasse"
+CollecObjectTrouve = "object_trouve"
+CollecTer = "ter"
+CollectTGV = "tgv"
 
 def insertMontParnasseToMongo():
     # appel la bdd "SNCF" pour faire les traitements vers la base mongo
-    getDataQuery = departMontparnasse(ApiKey,10)
+    getDataQuery = departMontparnasse(ApiKey, limitData)
     # connection mongoDB
     DBMongo = loadConfigDataBase()
     # insert des données
-    DBMongo.MontParnasse.insert_one(getDataQuery)
+    DBMongo[CollecMontParnasse].insert_many(getDataQuery['departures'])
+
+def insertObjetTrouveToMongo():
+    getDataQuery = objetTrouve(ApiKey, limitData)
+    DBMongo = loadConfigDataBase()
+    DBMongo[CollecObjectTrouve].insert_many(getDataQuery['records'])
+
+def insertTerMensuelToMongo():
+    getDataQuery = terMensuel(ApiKey, limitData)
+    DBMongo = loadConfigDataBase()
+    DBMongo[CollecTer].insert_many(getDataQuery['records'])
+
+def insertTGVMensuelToMongo():
+    getDataQuery = TGVMensuel(ApiKey, limitData)
+    DBMongo = loadConfigDataBase()
+    DBMongo[CollectTGV].insert_many(getDataQuery['records'])
 
 if __name__ == '__main__':
-    insertMontParnasseToMongo()
+    existMontParnasse = CheckCollection[CollecMontParnasse].count_documents({})
+    if existMontParnasse == 0:
+        insertMontParnasseToMongo()
+
+    existObjTrouve = CheckCollection[CollecObjectTrouve].count_documents({})
+    if existObjTrouve == 0:
+        insertObjetTrouveToMongo()
+
+    existTer = CheckCollection[CollecTer].count_documents({})
+    if existTer == 0:
+        insertTerMensuelToMongo()
+
+    existTgv = CheckCollection[CollectTGV].count_documents({})
+    if existTgv == 0:
+        insertTGVMensuelToMongo()
